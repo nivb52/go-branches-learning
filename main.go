6ba9eae961c152bd11b67dc7a9cb45f0233c4fbb
nivb52 <nivb52@gmail.com>
2023-03-02 20:24:01 +0200

    init todosController

--- main.go
+++ main.go
@@ -3,7 +3,71 @@ package main
 import (
 	"log"
 	"net/http"
+	"regexp"
 )
+
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusNotFound)
+    w.Write([]byte("not found"))
+}
+func notImplemented(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusNotImplemented)
+    w.Write([]byte("nnot implemented"))
+}
+
+func internalServerError(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("internal server error"))
+}
+
+// services.go TODOS SERVICE 
+func getTodos (rw http.ResponseWriter, rq *http.Request) {
+
+}
+func getTodoById (rw http.ResponseWriter, rq *http.Request) {}
+func createTodo (rw http.ResponseWriter, rq *http.Request) {}
+func updateTodo (rw http.ResponseWriter, rq *http.Request) {}
+func deleteTodo (rw http.ResponseWriter, rq *http.Request) {}
+
+
+
+// controllers.go TODOS CONTROLLER
+var (
+    listTodosRegex   = regexp.MustCompile(`^\/todos[\/]*$`)
+    todoRegex    = regexp.MustCompile(`^\/todos\/(\d+)$`)
+	createTodoRegex  = regexp.MustCompile(`^\/todos[\/]*$`)
+    createBatchTodosRegex = regexp.MustCompile(`^\/todos\/batch[\/]*$`)
+)
+
+func todosController(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("content-type", "application/json")
+	switch {
+		case r.Method == http.MethodGet  && listTodosRegex.MatchString(r.URL.Path):
+		 	getTodos(w, r)
+			return
+		case r.Method == http.MethodGet && todoRegex.MatchString(r.URL.Path):
+		 	getTodoById(w, r)
+			return
+		case r.Method == http.MethodDelete && todoRegex.MatchString(r.URL.Path):
+			deleteTodo(w, r)
+			return
+		case (r.Method == http.MethodPost || r.Method == http.MethodPatch) && todoRegex.MatchString(r.URL.Path):
+			updateTodo(w, r)
+			return
+		case (r.Method == http.MethodPost) && (createTodoRegex.MatchString(r.URL.Path)):
+			createTodo(w, r)
+			return
+		case (r.Method == http.MethodPost) && (createBatchTodosRegex.MatchString(r.URL.Path)):
+			notImplemented(w, r)
+			return
+		default:
+			notFound(w, r)
+			return
+		}
+}
+
+// main.go SERVER
 
 func hello(rw http.ResponseWriter, rq *http.Request) {
 	rw.Header().Set("Content-Type", "text/html")
@@ -15,9 +79,9 @@ func liveness(rw http.ResponseWriter, rq *http.Request) {
 }
 
 func main() {
-
-	http.HandleFunc("/", liveness)
-	http.HandleFunc("/hello", hello)
-
+	mux := http.NewServeMux()	
+	mux.HandleFunc("/todos/", todosController)
+	mux.HandleFunc("/hello", hello)
+	mux.HandleFunc("/", liveness)
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
